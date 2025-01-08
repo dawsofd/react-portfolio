a19be36a0a7c2a9d76c96e9deec8cd94fbd6d62c
Dawson Dohlen <d.dohlen@pm.me>
2025-01-07 18:29:10 -0800

    add PortfolioContainer.jsx

---
+++ src/components/PortfolioContainer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Resume from './pages/Resume/Resume';
+import Portfolio from './pages/Portfolio/Portfolio';
+
+export default function PortfolioContainer({ currentPage, setCurrentPage }) {
+    if (currentPage === 'About') {
+        return <About />;
+    }
+    if (currentPage === 'Contact') {
+        return <Contact />;
+    }
+    if (currentPage === 'Resume') {
+        return <Resume />;
+    }
+    return <Portfolio />;
+}
